751e8d25e1e23ec5c6dee50213f4cd6667f5812c
Shabbir414 <96943187+Shabbir414@users.noreply.github.com>
2022-04-02 04:59:10 +0500

    Home/arrival/featured
    Home & arrival & featured compleat

--- E_Cmrce_FationWebsite/Js/script.js
+++ E_Cmrce_FationWebsite/Js/script.js
@@ -20,5 +20,14 @@ $(document).ready(function() {
         dots: false,
         autoplay: true,
         autoplayTimeout: 8000,
+        loop: true,
+    });
+
+    $(".small-image img").click(function() {
+        $(this).addClass("image-active").siblings().removeClass("image-active");
+
+        let image = $(this).attr("src");
+
+        $(".big-image img").attr("src", image);
     });
 });
